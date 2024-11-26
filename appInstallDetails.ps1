64fe67fec03eca164d7ee540dfbb0a1c7c64e1db
trishika22 <81105231+trishika22@users.noreply.github.com>
2024-11-26 23:11:02 +0530

    Uploaded perSettingsReport.ps1 and  appInstallDetails.ps1

---
+++ appInstallDetails.ps1
@@ -0,0 +1,59 @@
+﻿Connect-MSGraph -AdminConsent
+
+$file = 'C:\temp\temp1.csv'
+
+$Body = @"
+{
+    "select": [
+        "DeviceName",
+        "UserPrincipalName",
+        "Platform",
+        "AppVersion",
+        "InstallState",
+        "InstallStateDetail",
+        "AssignmentFilterIdsExist",
+        "LastModifiedDateTime",
+        "DeviceId",
+        "ErrorCode",
+        "UserName",
+        "UserId",
+        "ApplicationId",
+        "AssignmentFilterIdsList",
+        "AppInstallState",
+        "AppInstallStateDetails",
+        "HexErrorCode"
+    ],
+    "skip": 0,
+    "filter": "(ApplicationId eq '082a60ea-0991-46d7-aff3-6d8e8dc037a0')",
+    "orderBy": []
+}
+"@
+
+
+
+forEach($value in (Invoke-MSGraphRequest -HttpMethod POST -Url "https://graph.microsoft.com/beta/deviceManagement/reports/getDeviceInstallStatusReport" -Content $Body).values)
+{
+    $output = [PSCustomObject]@{
+       DeviceName = $value[0]
+       UserPrincipalName = $value[1]
+        Platform = $value[2]
+        AppVersion = $value[3]
+        InstallState = $value[4]
+        InstallStateDetail = $value[5]
+        AssignmentFilterIdsExist = $value[6]
+        LastModifiedDateTime = $value[7]
+        DeviceId= $value[8]
+        ErrorCode = $value[9]
+        UserName = $value[10]
+        UserId = $value[11]
+        ApplicationId = $value[12]
+        AssignmentFilterIdsList = $value[13]
+        AppInstallState = $value[14]
+        AppInstallStateDetails = $value[15]
+        HexErrorCode = $value[16]
+    }
+
+    $output | Select-object DeviceName, UserPrincipalName, Platform, AppVersion, InstallState,InstallStateDetail,AssignmentFilterIdsExist,LastModifiedDateTime, DeviceId, ErrorCode, UserName, UserId, ApplicationId, AssignmentFilterIdsList, AppInstallState, AppInstallStateDetails, HexErrorCode | Export-Csv -Path $file -NoTypeInformation -Append
+    
+}
+exit 1
---
+++ perSettingsReport.ps1
@@ -0,0 +1,37 @@
+﻿$file = 'C:\temp\export1.csv'
+
+Connect-MSGraph -AdminConsent
+
+forEach($Device in Get-IntuneManageddevice){
+
+$Body = @"
+{ "select": [
+        "SettingName",
+        "SettingStatus",
+        "ErrorCode",
+        "SettingInstanceId",
+        "SettingInstancePath"
+    ],
+    "filter": "(PolicyId eq 'c6cb029a-e9cc-4416-90da-245b7af6a4d4') and (DeviceId eq '$($Device.id)') and (UserId eq '$($Device.userId)')",
+    "orderBy": []
+}
+"@
+
+forEach($value in (Invoke-MSGraphRequest -HttpMethod POST -Url "https://graph.microsoft.com/beta/deviceManagement/reports/microsoft.graph.getConfigurationSettingNonComplianceReport" -Content $Body).values)
+{
+    $output = [PSCustomObject]@{
+        DeviceId = $Device.id
+        UserId = $Device.userId
+        ErrorCode = $value[0] 
+        SettingInstanceId = $value[1]
+        SettingInstancePath = $value[2]
+        SettingName = $value[3]
+        SettingStatus = $value[4]
+
+    }
+
+    $output | Select-object DeviceId, UserId, ErrorCode, SettingInstanceId, SettingInstancePath,SettingName,SettingStatus | Export-Csv -Path $file -NoTypeInformation -Append
+    
+}
+
+}
